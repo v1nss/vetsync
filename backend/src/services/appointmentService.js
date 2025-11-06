6f4eaaf35c197301c81aa67a5ba3513aa0f3cb08
v1nss <vincemarc.mr@gmail.com>
2025-11-07 07:28:25 +0800

    Fixed acceptAppointment bug

--- backend/src/services/appointmentService.js
+++ backend/src/services/appointmentService.js
@@ -11,25 +11,28 @@ export const acceptAppointment = async (clinicAdminId, appointmentId, vetProId) 
   const appointment = await Appointment.findByPk(appointmentId);
   if (!appointment) throw new Error("Appointment not found");
 
-    const vetProExists = await VetProfessional.findOne({
+  const vetProExists = await VetProfessional.findOne({
     where: {
       user_id: vetProId,
       clinic_admin_id: clinicAdminId,
     },
-  });// need to double check restriction logic // cannot handle multiple clinics for now
-    if (!vetProExists) {
-        throw new Error("Vet Professional does not belong to this Clinic"); // does not belong to clinic admin
-    }
-  appointment.status = "confirmed";
-  appointment.vet_professional_id = vetProId;
- 
-  await appointment.update(appointment);
+  });
+
+  if (!vetProExists) {
+    throw new Error("Vet Professional does not belong to this Clinic");
+  }
+
+  await appointment.update({
+    status: "confirmed",
+    vet_professional_id: vetProId,
+  });
+
   return appointment;
 };
 
 export const completeAppointment = async (appointmentId) => {
- await Appointment.update(
-  { status: "completed" },
-  { where: { appointment_id: appointmentId } }
-);
+  await Appointment.update(
+    { status: "completed" },
+    { where: { appointment_id: appointmentId } }
+  );
 }
