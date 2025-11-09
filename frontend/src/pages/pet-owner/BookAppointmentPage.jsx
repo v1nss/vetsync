4083e40e5bdff48af14f34baeb770437dd13e409
v1nss <vincemarc.mr@gmail.com>
2025-11-10 00:55:56 +0800

    Created a frontend for settings page

--- frontend/src/pages/pet-owner/BookAppointmentPage.jsx
+++ frontend/src/pages/pet-owner/BookAppointmentPage.jsx
@@ -17,30 +17,30 @@ export default function BookAppointmentPage() {
   const clinic = location.state?.clinic;
 
   const [pets] = useState([
-      {
-          id: 1,
-          name: "Brownie",
-          age: "8 years old",
-          weight: "12 kilos",
-          breed: "Aspin",
-          gender: "Male"
-      },
-      {
-          id: 2,
-          name: "Cici",
-          age: "3 years old",
-          weight: "25 kilos",
-          breed: "Golden Retriever",
-          gender: "Female"
-      },
-      {
-          id: 3,
-          name: "Dynamo",
-          age: "3 years old",
-          weight: "28 kilos",
-          breed: "Golden Retriever",
-          gender: "Male"
-      },
+    {
+      id: 1,
+      name: "Brownie",
+      age: "8 years old",
+      weight: "12 kilos",
+      breed: "Aspin",
+      gender: "Male",
+    },
+    {
+      id: 2,
+      name: "Cici",
+      age: "3 years old",
+      weight: "25 kilos",
+      breed: "Golden Retriever",
+      gender: "Female",
+    },
+    {
+      id: 3,
+      name: "Dynamo",
+      age: "3 years old",
+      weight: "28 kilos",
+      breed: "Golden Retriever",
+      gender: "Male",
+    },
   ]);
 
   if (!clinic) {
@@ -74,171 +74,178 @@ export default function BookAppointmentPage() {
 
   return (
     <main>
-        <div className="hidden sm:block">
+      <div className="hidden sm:block">
         <Navbar />
+      </div>
+
+      <div className="max-w-5xl mx-auto px-4 pt-20 pb-10 sm:pt-8">
+        <div className="top-0 sm:hidden fixed left-0 p-4 z-50 flex items-center gap-2 bg-white w-full border-b border-gray-300">
+          <button
+            onClick={() => navigate(-1)}
+            className="rounded-full hover:bg-gray-300 transition"
+          >
+            <FaChevronLeft className="text-gray-500" />
+          </button>
+          <h1 className="text-xl font-medium">Book Appointment</h1>
         </div>
 
-        <div className="max-w-5xl mx-auto px-4 pt-24 pb-10 sm:pt-8">
-            <div className="top-0 sm:hidden fixed left-0 p-4 z-50 flex items-center gap-2 bg-white w-full border-b border-gray-300">
+        <div className="border border-gray-300 rounded-2xl p-6 mb-4 m:mb-8">
+          <h1 className="text-xl font-semibold mb-3">{clinic.name}</h1>
+
+          <div className="space-y-2 text-gray-700 text-sm">
+            <p className="flex items-start gap-2">
+              <FaLocationDot className="mt-[3px]" />
+              {clinic.address}
+            </p>
+
+            {clinic.contact && (
+              <p className="flex items-center gap-2">
+                <FaPhoneAlt />
+                {clinic.contact}
+              </p>
+            )}
+
+            {clinic.openingHours && (
+              <p className="flex items-center gap-2">
+                <FaClock />
+                {clinic.openingHours}
+              </p>
+            )}
+          </div>
+        </div>
+
+        {/* Booking Form */}
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-6 border border-gray-300 rounded-2xl p-6"
+        >
+          <h2 className="font-semibold text-lg">Book Appointment</h2>
+
+          {/* Select Pet */}
+          <div className="flex flex-col gap-2">
+            <label className="label-required text-sm font-medium">
+              Select Pet
+            </label>
+
+            <div className="flex flex-wrap gap-3">
+              {pets.map((pet) => (
                 <button
-                    onClick={() => navigate(-1)}
-                    className="rounded-full p-3 hover:bg-gray-300 transition"
-                >
-                    <FaChevronLeft className="text-gray-500" />
-                </button>
-                <h1 className="text-xl font-medium">Book Appointment</h1>
-            </div>
-
-            <div className="border border-gray-300 rounded-2xl p-6 mb-8">
-                <h1 className="text-xl font-semibold mb-3">{clinic.name}</h1>
-
-                <div className="space-y-2 text-gray-700 text-sm">
-                <p className="flex items-start gap-2">
-                    <FaLocationDot className="mt-[3px]" />
-                    {clinic.address}
-                </p>
-
-                {clinic.contact && (
-                    <p className="flex items-center gap-2">
-                    <FaPhoneAlt />
-                    {clinic.contact}
-                    </p>
-                )}
-
-                {clinic.openingHours && (
-                    <p className="flex items-center gap-2">
-                    <FaClock />
-                    {clinic.openingHours}
-                    </p>
-                )}
-                </div>
-            </div>
-
-            {/* Booking Form */}
-            <form
-                onSubmit={handleSubmit}
-                className="space-y-6 border border-gray-300 rounded-2xl p-6"
-            >
-                <h2 className="font-semibold text-lg">Book Appointment</h2>
-
-                {/* Select Pet */}
-                <div className="flex flex-col gap-2">
-                  <label className="label-required text-sm font-medium">Select Pet</label>
-
-                  <div className="flex flex-wrap gap-3">
-                    {pets.map((pet) => (
-                      <button
-                        key={pet.id}
-                        type="button"
-                        onClick={() => setSelectedPet(pet.name)}
-                        className={`flex flex-col items-center p-3 rounded-xl border w-[90px]
+                  key={pet.id}
+                  type="button"
+                  onClick={() => setSelectedPet(pet.name)}
+                  className={`flex flex-col items-center p-3 rounded-xl border w-[90px]
                           transition ${
                             selectedPet === pet.name
                               ? "border-primary bg-primary text-white"
                               : "border-gray-300 text-gray-700"
                           }`}
-                      >
-                        <img
-                          src={pet.photo}
-                          alt={pet.name}
-                          className="w-14 h-14 object-cover rounded-full"
-                        />
-                        <span className="text-xs font-medium mt-2">{pet.name}</span>
-                      </button>
-                    ))}
-                  </div>
-
-                  {/* Hidden input for form submission */}
-                  <input type="hidden" required value={selectedPet} />
-                </div>
-
-                {/* Service */}
-                <div className="flex flex-col gap-2">
-                  <label className="label-required text-sm font-medium">Select Service</label>
-
-                  {/* Buttons container */}
-                  <div className="flex flex-wrap gap-2">
-                    {clinic.services?.map((service) => (
-                      <button
-                        key={service}
-                        type="button"
-                        onClick={() => setSelectedService(service)}
-                        className={`px-4 py-2 rounded-xl border text-sm
-                          ${selectedService === service
-                            ? "bg-primary text-white border-primary"
-                            : "border-gray-300 text-gray-700"}`}
-                      >
-                        {service}
-                      </button>
-                    ))}
-
-                    {/* "Others" button */}
-                    <button
-                      type="button"
-                      onClick={() => setSelectedService("others")}
-                      className={`px-4 py-2 rounded-xl border text-sm
-                        ${selectedService === "others"
-                          ? "bg-primary text-white border-primary"
-                          : "border-gray-300 text-gray-700"}`}
-                    >
-                      Others
-                    </button>
-                  </div>
-
-                  {/* Show input field only when selecting "Others" */}
-                  {selectedService === "others" && (
-                    <input
-                      type="text"
-                      placeholder="Specify other service"
-                      onChange={(e) => setOtherService(e.target.value)}
-                      className="border p-3 rounded-xl mt-2"
-                      required
-                    />
-                  )}
-                </div>
-
-                {/* Date */}
-                <div className="flex flex-col gap-1">
-                <label className="label-required text-sm font-medium">Date</label>
-                <input
-                    type="date"
-                    onChange={(e) => setAppointmentDate(e.target.value)}
-                    required
-                    className="border border-gray-300 p-3 rounded-xl"
-                />
-                </div>
-
-                {/* Time */}
-                <div className="flex flex-col gap-1">
-                <label className="label-required text-sm font-medium">Time</label>
-                <input
-                    type="time"
-                    onChange={(e) => setAppointmentTime(e.target.value)}
-                    required
-                    className="border border-gray-300 p-3 rounded-xl"
-                />
-                </div>
-
-                {/* Reason */}
-                <div className="flex flex-col gap-1">
-                <label className="text-sm font-medium">Reason (optional)</label>
-                <textarea
-                    placeholder="Enter reason"
-                    onChange={(e) => setReason(e.target.value)}
-                    className="border border-gray-300 p-3 rounded-xl min-h-20 resize-none"
-                />
-                </div>
-
-                {/* Submit */}
-                <button
-                type="submit"
-                className="w-full bg-primary text-white py-3 rounded-xl font-semibold"
                 >
-                Confirm appointment
+                  <img
+                    src={pet.photo}
+                    alt={pet.name}
+                    className="w-14 h-14 object-cover rounded-full"
+                  />
+                  <span className="text-xs font-medium mt-2">{pet.name}</span>
                 </button>
-                
-            </form>
-        </div>
+              ))}
+            </div>
+
+            {/* Hidden input for form submission */}
+            <input type="hidden" required value={selectedPet} />
+          </div>
+
+          {/* Service */}
+          <div className="flex flex-col gap-2">
+            <label className="label-required text-sm font-medium">
+              Select Service
+            </label>
+
+            {/* Buttons container */}
+            <div className="flex flex-wrap gap-2">
+              {clinic.services?.map((service) => (
+                <button
+                  key={service}
+                  type="button"
+                  onClick={() => setSelectedService(service)}
+                  className={`px-4 py-2 rounded-xl border text-sm
+                          ${
+                            selectedService === service
+                              ? "bg-primary text-white border-primary"
+                              : "border-gray-300 text-gray-700"
+                          }`}
+                >
+                  {service}
+                </button>
+              ))}
+
+              {/* "Others" button */}
+              <button
+                type="button"
+                onClick={() => setSelectedService("others")}
+                className={`px-4 py-2 rounded-xl border text-sm
+                        ${
+                          selectedService === "others"
+                            ? "bg-primary text-white border-primary"
+                            : "border-gray-300 text-gray-700"
+                        }`}
+              >
+                Others
+              </button>
+            </div>
+
+            {/* Show input field only when selecting "Others" */}
+            {selectedService === "others" && (
+              <input
+                type="text"
+                placeholder="Specify other service"
+                onChange={(e) => setOtherService(e.target.value)}
+                className="border p-3 rounded-xl mt-2"
+                required
+              />
+            )}
+          </div>
+
+          {/* Date */}
+          <div className="flex flex-col gap-1">
+            <label className="label-required text-sm font-medium">Date</label>
+            <input
+              type="date"
+              onChange={(e) => setAppointmentDate(e.target.value)}
+              required
+              className="border border-gray-300 p-3 rounded-xl"
+            />
+          </div>
+
+          {/* Time */}
+          <div className="flex flex-col gap-1">
+            <label className="label-required text-sm font-medium">Time</label>
+            <input
+              type="time"
+              onChange={(e) => setAppointmentTime(e.target.value)}
+              required
+              className="border border-gray-300 p-3 rounded-xl"
+            />
+          </div>
+
+          {/* Reason */}
+          <div className="flex flex-col gap-1">
+            <label className="text-sm font-medium">Reason (optional)</label>
+            <textarea
+              placeholder="Enter reason"
+              onChange={(e) => setReason(e.target.value)}
+              className="border border-gray-300 p-3 rounded-xl min-h-20 resize-none"
+            />
+          </div>
+
+          {/* Submit */}
+          <button
+            type="submit"
+            className="w-full bg-primary text-white py-3 rounded-xl font-semibold"
+          >
+            Confirm appointment
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
