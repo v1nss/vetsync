7e7569a8dd7f8561e5e34ad22efb377970f8a212
v1nss <vincemarc.mr@gmail.com>
2025-11-24 06:32:09 +0800

    fixed typo in env pet.jsx

--- frontend/src/global/api/pet.jsx
+++ frontend/src/global/api/pet.jsx
@@ -1,13 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_BACKEN_URL;
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const registerPet = async (token, petData) => {
-    for (let [key, value] of petData.entries()) {
-    console.log(key, value);
-    }
     try {
-        const res = await axios.post(`http://localhost:4000/api/pets/register`, petData, {
+        const res = await axios.post(`${BASE_URL}/pets/register`, petData, {
             headers: {
                 "Authorization": `Bearer ${token}`,
                 "Content-Type": "multipart/form-data"
