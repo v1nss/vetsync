9a19cf68196d66589dafdf9b6c30f587a9269921
v1nss <vincemarc.mr@gmail.com>
2025-12-09 06:02:51 +0800

    fetching prescription by petowner restricted

--- frontend/src/global/utils/api.jsx
+++ frontend/src/global/utils/api.jsx
@@ -1,7 +1,7 @@
 // api.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL = import.meta.env.VITE_LOCAL_BACKEND_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
