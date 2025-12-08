d2cce09c7a1e71d92b33c7274e90382f1a52fa83
v1nss <vincemarc.mr@gmail.com>
2025-12-08 18:34:46 +0800

    modified db configuration

--- backend/global/config/db.js
+++ backend/global/config/db.js
@@ -2,13 +2,33 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
+// const sequelize = new Sequelize(
+//   process.env.DB_DATABASE,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//     logging: false,
+//     pool: {
+//       max: 10,        // maximum number of connections
+//       min: 0,
+//       acquire: 30000, // wait 30s before throwing error
+//       idle: 10000,    // close idle connections after 10s
+//     },
+//   }
+// );
+
+// export default sequelize;
+
 const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
     dialect: "postgres",
     logging: false,
     pool: {
