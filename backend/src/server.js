0fa3979e1940c92a761721016ebdcb1442764ae9
v1nss <vincemarc.mr@gmail.com>
2026-03-29 14:00:12 +0800

    removed /api to endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,31 +46,31 @@ app.use(cookieParser());
 app.use(auditLogger);
 
 //user Routes
-app.use("/api/users", userRoutes);
+app.use("/users", userRoutes);
 
 //auth Routes
-app.use('/api/auth', authRoutes);
+app.use('/auth', authRoutes);
 
 //pet Routes
-app.use("/api/pets", petRoutes);
+app.use("/pets", petRoutes);
 
 //clinic Routes
-app.use("/api/clinics", clinicRoutes);
+app.use("/clinics", clinicRoutes);
 
 // appointment Routes
-app.use("/api/appointments", appointmentRoutes);
+app.use("/appointments", appointmentRoutes);
 
 //system admin Routes
-app.use("/api/system-admin", systemAdminRoutes);
+app.use("/system-admin", systemAdminRoutes);
 
-app.use("/api/approval-logs", ApprovalLogRoutes);
+app.use("/approval-logs", ApprovalLogRoutes);
 
 // EHR Routes
-app.use("/api/ehr", ehrRoutes);
-app.use("/api/clinic-patients", clinicPatientRoutes);
+app.use("/ehr", ehrRoutes);
+app.use("/clinic-patients", clinicPatientRoutes);
 
 // Reports Routes
-app.use("/api/reports", reportsRoutes);
+app.use("/reports", reportsRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
