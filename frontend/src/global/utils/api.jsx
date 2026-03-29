29903d6d4f5a7d41e054d19bc7de526b953f754a
v1nss <vincemarc.mr@gmail.com>
2026-03-29 13:14:47 +0800

    modified api interceptor to use rewrites

--- frontend/src/global/utils/api.jsx
+++ frontend/src/global/utils/api.jsx
@@ -1,10 +1,10 @@
 // api.js
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+// const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const api = axios.create({
-  baseURL: `${BASE_URL}/api`,
+  baseURL: "/api",
   withCredentials: true, // Send cookies with every request
 });
 
